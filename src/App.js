722055f7dfb31d5b210485283e453914baba63b2
Maliha <maliha_786@hotmail.com>
2018-09-10 15:24:55 -0700

    Add style with and without Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+// import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -54,11 +55,12 @@ class App extends Component {
 
   render() {
     const style= {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
     }
 
     let persons = null;
@@ -76,17 +78,31 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <=1) {
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>Maliha's React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>Maliha's React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonHandler}>Toggle Persons</button>
+          {persons}
+        </div>
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Person from './Person.css';
+// import Radium from 'radium';
 
 const person = (props) => {
+  // const style = {
+  //   '@media (min-width: 500px)': {
+  //     width: '450px'
+  //   }
+  // }
    return (
      <div className="Person">
        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
